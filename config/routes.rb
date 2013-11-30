10ba767f9df6591918680382592d31973b69cc09
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-11-30 10:44:52 -0800

    Add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  resources :users
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
