2ce3f90b88ec34acd4a2a116e24fff6f2048b18d
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-12-01 15:31:14 -0800

    Add Sign in and out and the tests for them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -24,6 +25,12 @@ describe User do
                      password: " ", password_confirmation: " ")
     end
     it { should_not be_valid }
+  
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
   
   describe "when password doesn't match confirmation" do
